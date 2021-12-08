6c0422be9f96e5bfac2ed1a26d3e14969c9c1788
Pablo Santos <pablo.roldao.santos@gmail.com>
2021-12-08 14:44:14 -0300

    arrumando o carrosel da home

--- home.php
+++ home.php
@@ -78,45 +78,33 @@
 
     </div>
 
-    <p id="apresentacao-carrosel" class="text-center bg-dark bg-opacity-75 rounded shadow-lg fw-bold" >Vídeos para aprender sobre o emagrecimento coletivo:</p>
-    <div id="carouselVideo" class="carousel slide rounded shadow-lg" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselVideo" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselVideo" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselVideo" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
+    <p id="apresentacao-carrosel" class="text-center bg-dark bg-opacity-75 rounded shadow-lg fw-bold">Vídeos para aprender sobre o emagrecimento coletivo:</p>
+
+    <div id="carouselExampleControls" class="carousel slide text-center bg-dark bg-opacity-75 rounded shadow-lg " data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="imagens/walking-g459c72270_1920.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Vídeo 01</h5>
-                    <p>Legenda 01...</p>
-                </div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/wwRY3o7eoLM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <div class="carousel-item">
-                <img src="imagens/gabin-vallet-J154nEkpzlQ-unsplash.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Vídeo 02...</h5>
-                    <p>Some representative placeholder content for the second slide.</p>
-                </div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/sQeolmlMFlg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Some representative placeholder content for the third slide.</p>
-                </div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/F2d6nNjxjbQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+            <span class="sr-only">Próximo</span>
+        </a>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
 
