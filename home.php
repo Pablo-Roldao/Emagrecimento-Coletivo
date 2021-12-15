f1d468a27e6e1de548de91962f36b3b92a3fc655
Pablo Santos <pablo.roldao.santos@gmail.com>
2021-12-15 14:54:08 -0300

    adcionando um carrosel a home

--- home.php
+++ home.php
@@ -78,35 +78,30 @@
 
     </div>
 
-    <!--
-    <p id="apresentacao-carrosel" class="text-center bg-dark bg-opacity-75 rounded shadow-lg fw-bold">Vídeos para aprender sobre o emagrecimento coletivo:</p>
-
     <div id="carouselExampleControls" class="carousel slide text-center bg-dark bg-opacity-75 rounded shadow-lg " data-ride="carousel">
+    <p id="apresentacao-carrosel" class="text-center fw-bold">Vídeos para aprender sobre o emagrecimento coletivo:</p>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/wwRY3o7eoLM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="660" height="394" src="https://www.youtube.com/embed/wwRY3o7eoLM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <div class="carousel-item">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/sQeolmlMFlg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="700" height="394" src="https://www.youtube.com/embed/sQeolmlMFlg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <div class="carousel-item">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/F2d6nNjxjbQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="700" height="394" src="https://www.youtube.com/embed/F2d6nNjxjbQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Anterior</span>
         </a>
         <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Próximo</span>
         </a>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    -->
 
 </body>
 
